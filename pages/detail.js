2afa23e9bbad6d70254fb934a565d52446a6bb5c
uniquexiaobai <uniquexiaobai@gmail.com>
2020-03-01 16:14:20 +0800

    lazy img

--- pages/detail.js
+++ pages/detail.js
@@ -1,40 +1,40 @@
 import React from 'react';
 import Head from 'next/head';
 
-const Detail = ({id}) => {
-    const url = `https://unique-bing.oss-cn-beijing.aliyuncs.com/${id}.jpeg`;
+const Detail = ({ id }) => {
+	const url = `https://unique-bing.oss-cn-beijing.aliyuncs.com/${id}.jpeg`;
 
-    return (
-        <div className="detail">
-            <Head>
-                <title>Detail</title>
-            </Head>
+	return (
+		<div className='detail'>
+			<Head>
+				<title>Detail</title>
+			</Head>
 
-            <img src={url} alt={id} />
+			<img src={url} alt={id} />
 
-            <style jsx>{`
-                .detail {
-                    position: absolute;
-                    left: 0;
-                    right: 0;
-                    top: 0;
-                    bottom: 0;
-                }
-                img {
-                    width: 100%;
-                    height: 100%;
-                    vertical-align: middle;
-                    object-fit: cover;
-                }
-            `}</style>
-        </div>
-    );
+			<style jsx>{`
+				.detail {
+					position: absolute;
+					left: 0;
+					right: 0;
+					top: 0;
+					bottom: 0;
+				}
+				img {
+					width: 100%;
+					height: 100%;
+					vertical-align: middle;
+					object-fit: cover;
+				}
+			`}</style>
+		</div>
+	);
 };
 
-Detail.getInitialProps = ({query}) => {
-    const {id} = query;
+Detail.getInitialProps = ({ query }) => {
+	const { id } = query;
 
-    return {id};
+	return { id };
 };
 
 export default Detail;
--- pages/index.js
+++ pages/index.js
@@ -1,87 +1,101 @@
 import React from 'react';
 import Head from 'next/head';
 import Link from 'next/link';
-import {format, subDays, differenceInDays} from 'date-fns';
+import { format, subDays, differenceInDays } from 'date-fns';
+import { LazyLoadImage } from 'react-lazy-load-image-component';
 import Pagination from '../components/pagination';
 
+import 'react-lazy-load-image-component/src/effects/blur.css';
 import '../styles/base.css';
 
 const getIdList = (page = 1) => {
-    const now = new Date();
-    const offset = 30 * (page - 1);
-    const formatStr = 'yyyyMMdd';
-    const list = Array.from({length: 30}, (_, i) => format(subDays(now, i + offset), formatStr));
+	const now = new Date();
+	const offset = 30 * (page - 1);
+	const formatStr = 'yyyyMMdd';
+	const list = Array.from({ length: 30 }, (_, i) =>
+		format(subDays(now, i + offset), formatStr)
+	);
 
-    return list;
+	return list;
 };
 
-const Home = ({page}) => {
-    const baseUrl = 'https://unique-bing.oss-cn-beijing.aliyuncs.com/';
-    const totalPage = Math.floor(differenceInDays(new Date(), new Date(2017, 0, 1)) / 30);
-    const list = getIdList(page);
+const Home = ({ page }) => {
+	const baseUrl = 'https://unique-bing.oss-cn-beijing.aliyuncs.com/';
+	const totalPage = Math.floor(
+		differenceInDays(new Date(), new Date(2017, 0, 1)) / 30
+	);
+	const list = getIdList(page);
 
-    return (
-        <div>
-            <Head>
-                <title>Home</title>
-            </Head>
+	return (
+		<div>
+			<Head>
+				<title>Home</title>
+			</Head>
 
-            <div className='home'>
-                <div className='title'>
-                    <h1>必应图片</h1>
-                </div>
+			<div className='home'>
+				<div className='title'>
+					<h1>必应图片</h1>
+				</div>
 
-                <ul>
-                    {list.map((id) => (
-                        <li key={id}>
-                            <Link href={{pathname: '/detail', query: {id}}}>
-                                <img src={`${baseUrl}${id}.jpeg!s`} alt={id} />
-                            </Link>
-                        </li>
-                    ))}
-                </ul>
-            </div>
+				<ul>
+					{list.map(id => (
+						<li key={id}>
+							<Link href={{ pathname: '/detail', query: { id } }}>
+								<LazyLoadImage
+									src={`${baseUrl}${id}.jpeg!s`}
+									alt={id}
+									effect='blur'
+									wrapperClassName='wrapper'
+								/>
+							</Link>
+						</li>
+					))}
+				</ul>
+			</div>
 
-            <Pagination total={totalPage} page={page} />
+			<Pagination total={totalPage} page={page} />
 
-            <style jsx>{`
-                .home {
-                    margin-bottom: 10px;
-                }
-                .title {
-                    text-align: center;
-                    color: white;
-                    background: rebeccapurple;
-                    padding: 1px;
-                }
-                li {
-                    display: inline-block;
-                    width: calc(100% / 3);
-                    margin: 0;
-                }
-                @media only screen and (max-width: 980px) {
-                    li {
-                        width: 50%;
-                    }
-                }
-                @media only screen and (max-width: 640px) {
-                    li {
-                        width: 100%;
-                    }
-                }
-                img {
-                    width: 100%;
-                    vertical-align: middle;
-                }
-            `}</style>
-        </div>
-    );
+			<style jsx>{`
+				.home {
+					margin-bottom: 10px;
+				}
+				.title {
+					text-align: center;
+					color: white;
+					background: rebeccapurple;
+					padding: 1px;
+				}
+				li {
+					display: inline-block;
+					width: calc(100% / 3);
+					height: auto;
+					margin: 0;
+				}
+				@media only screen and (max-width: 980px) {
+					li {
+						width: 50%;
+					}
+				}
+				@media only screen and (max-width: 640px) {
+					li {
+						width: 100%;
+					}
+				}
+			`}</style>
+			<style global jsx>{`
+				.wrapper img {
+					width: 100%;
+					vertical-align: middle;
+				}
+			`}</style>
+		</div>
+	);
 };
 
-Home.getInitialProps = ({query}) => {
-    const {page = 1} = query;
+Home.getInitialProps = ({ query }) => {
+	const { page = 1 } = query;
 
-    return {page};
+	return { page };
 };
 
 export default Home;
